df3640b33ca4d3eb04a7962598a2424efc0768ce
shohan12727 <aishohan001@gmail.com>
2025-05-05 12:56:16 +0600

    inheritance practice

---
+++ ColorCode/inheritance.js
@@ -0,0 +1,46 @@
+class person {
+    talk () {
+        return 'talking'
+    }
+}
+const me = new person () ;
+const you = new person () ;
+// console.log(me.talk());
+// console.log(you.talk());
+
+// for (let i=0; i<1000; i++){
+//     console.log('fuck cse');
+// }
+
+
+class fd {
+    gandus () {
+        return "gandu the  gay "
+    }
+}
+
+const pal = new fd ();
+const rajesh = new fd ();
+// console.log(pal.gandus());
+// console.log(rajesh.gandus());
+
+
+class personn {
+    talk () {
+        return 'talking';
+    }
+}
+
+const shohan = new personn;
+const simanto = new personn ;
+const rajeshh = new personn ;
+const pall = new personn ;
+
+console.log(shohan.talk());
+console.log(simanto.talk());
+console.log(rajeshh.talk());
+console.log(pall.talk());
+
+
+
+
