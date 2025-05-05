48538cd6fc60099dd4e7bb8fe9a4d6d84e41e344
shohan12727 <aishohan001@gmail.com>
2025-05-05 22:14:59 +0600

    console a list anchi

--- module25/dom.js
+++ module25/dom.js
@@ -7,3 +7,59 @@ for (const li of liCollection){
 
 const dom = document.getElementsByTagName('h1')     
 console.log(dom[0].innerText); 
+
+const countryname = document.getElementById('country').innerText('uganda');
+console.log(countryname);
+
+document.getElementById('country').innerText = 'uganda';
+const countryname = 'uganda';
+console.log(countryname);
+
+
+document.getElementById('country').innerText = 'Uganda';
+const countryname = 'uganda'
+console.log(countryname);
+
+const animalName = document.getElementsByClassName('animals-lists').innerText;
+for (const element of animalName){
+    console.log(element);   
+}
+const contriesList = document.getElementsByClassName('contries-list').innerText;
+for (const element of contriesList){
+    console.log(element);
+}
+
+
+
+Get the animal list items
+const animalItems = document.querySelectorAll('.animals-lists ul li');
+for (const element of animalItems) {
+    console.log(element.innerText);
+}
+
+Get the country list items
+const countryItems = document.querySelectorAll('.contries-list ul li');
+for (const element of countryItems) {
+    console.log(element.innerText);
+}
+
+const animalsItems = document.querySelectorAll('.animals-lists ul li');
+for (const element of animalsItems){
+    console.log(element.innerText)
+}
+
+const countryLists = document.querySelectorAll('.contries-list ul li')
+for (const element of countryLists){
+    console.log(element.innerText);    
+}
+
+const  districtItems =  document.querySelectorAll('.district-lists ul li');
+for(const element of districtItems) {
+    console.log(element.innerText);
+}
+
+const fruitsLists = document.querySelectorAll('.fruits-list ul li');
+for (const element of fruitsLists ){
+    console.log(element.innerText);
+}
+
