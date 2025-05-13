c28c86a45149252e873f296b585f2ba4e5d24077
shohan12727 <aishohan001@gmail.com>
2025-05-13 22:06:22 +0600

    iife

--- Advance js/main.js
+++ Advance js/main.js
@@ -1,19 +1,85 @@
-// global scope  
+// global scope
 
-let x = 1 ;
+let x = 1;
 
-const parentFunction = () =>{
-  let myValue = 2 ;
+const parentFunction = () => {
+  let myValue = 2;
   console.log(x);
   console.log(myValue);
 
-   const childFunction = () => {
-    console.log(x += 10);
-    console.log(myValue += 5);
-    
-   }
-  
-   childFunction();
+  const childFunction = () => {
+    console.log((x += 10));
+    console.log((myValue += 5));
+  };
+
+  return childFunction;
+};
+
+const result = parentFunction();
+
+console.log(result);
+
+result () ;
+
+result () ;
+
+// IIFE (immediately invoked Function Expression)
+
+
+
+const privateCounter = (() => {
+   let count =0 ;
+   console.log(`initial value: ${count}`);
+   return () => {count += 1; console.log(count);}
+   
+}) () ;
+
+privateCounter() ;
+privateCounter() ;
+privateCounter() ;
+privateCounter() ;
+privateCounter() ;
+privateCounter() ;
+
+
+
+(function () {
+  let name = "Shohan";
+  console.log("Hello, " + name);
+})();
+
+
+( function () {
+  let name = 'Shohan';
+  console.log("Ashraful Islam " + name);
+}
+)();
+
+
+
+
+
+
+
+
+
+
+
+
+( () => {
+  let food = '🍕'
+  console.log('Food I like most is: ' + food);
 }
 
-parentFunction();
+) ();
+
+
+
+
+
+
+
+
+
+
+
